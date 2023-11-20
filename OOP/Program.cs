364706866667db1f1afa15c29602994dd8ccf509
Fırat Işıldak <firatisildak1@gmail.com>
2023-11-20 18:36:40 +0000

    first commit

---
+++ OOP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OOP // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ıhtiyacKrediManager= new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager =new KonutKrediManager();
+            IKrediManager esnafKrediManager = new EsnafKredisiManager();
+
+            ILoggerService databaseLoggerService=new DatabeseLoggerService();
+            ILoggerService fileLogerService=new FileLoggerService();
+
+            List<ILoggerService> loggers = new List<ILoggerService> { new SmsLoggerService(), new FileLoggerService()};
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(esnafKrediManager, loggers);
+            List<IKrediManager> krediler= new List<IKrediManager>() { ıhtiyacKrediManager, tasitKrediManager };
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
